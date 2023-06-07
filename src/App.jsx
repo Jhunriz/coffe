1facfe606ab3d9dcbed8ec655539e3b192cf0812
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2023-06-08 00:20:57 +0800

    add icon in searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import bg from "./image/bg.jpg";
 import "./App.css";
 import NavBars from "./components/NavBars";
-import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Buttom from "./components/bottom";
+import Bottom from "./components/bottom";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -26,7 +26,7 @@ function App() {
             <Route path="/pricing" element={<Pricing />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
           </Routes>
-          <Buttom />
+          <Bottom />
         </BrowserRouter>
       </div>
     </>
