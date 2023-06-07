d86578a674e0e14d20dafea6d2bdddd4271ffb91
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2023-06-08 00:05:42 +0800

    add-content and design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,19 @@ import { Slide } from "react-awesome-reveal";
 export default function About() {
   return (
     <Slide>
-      <div className="my-24">
-        <h1>welcome User</h1>
+      <div className="my-48 flex flex-col md:flex-row md:justify-start md:items-center md:gap-5 mx-4 md:mx-96">
+        <img
+          className="h-96 w-96 md:h-96 md:w-96"
+          src="https://scontent.fmnl17-4.fna.fbcdn.net/v/t39.30808-6/340763553_937654450717223_3773237026733644694_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG5gjOoDxTuDKJY4GYnCXYeU17ugjPkJzdTXu6CM-QnN_BG6Q10TrMGtWheegls3QR7-tEtercCYnIJAscWNSTL&_nc_ohc=e0ru8EY0OWsAX-FhZ3V&_nc_ht=scontent.fmnl17-4.fna&oh=00_AfAs0bKjU7OigGyepTk3mXI692rRdEX6tmXP5rAgJnWx8Q&oe=6485FDAF"
+          alt="image description"
+        />
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+          cum vitae eaque iure consectetur. Porro officiis ipsa quo nulla
+          consequatur, totam cumque assumenda distinctio odio magnam libero
+          molestiae commodi facilis?
+        </p>
       </div>
     </Slide>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,14 +6,14 @@ export default function Homepage() {
   return (
     <>
       <Slide>
-        <div className="flex flex-col items-start gap-5 justify-start py-10 px-6 md:flex-col md:items-start md:justify-start lg:py-20 lg:px-12 xl:py-48 xl:px-24">
+        <div className="flex flex-col items-start gap-5 justify-start my-20 px-6 md:flex-col md:items-start md:justify-start lg:py-20 lg:px-12 xl:py-48 xl:px-24">
           <p className="text-3xl md:text-5xl">Bukangliwayway</p>
           <p className="md:text-lg xl:text-sm">
             "Bukang-liwayway" is a Tagalog word that can be translated into
             English as "dawn"
           </p>
           <p className="text-sm">"daybreak", or "break of day".</p>
-          <Button gradientDuoTone="tealToLime">
+          <Button className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-gradient-to-r from-black to-yellow-950 hover:from-yellow-900 hover:to-black">
             Learn More
             <svg
               xmlns="http://www.w3.org/2000/svg"
